9f87bf8c3a4f030137727d64410149e97e488047
Liva Joensen <livalinea@gmail.dk>
2025-11-29 12:10:07 +0100

    puttet i folders

--- Phoenix/Phoenix/View/Member/AddMemberInfo.xaml.cs
+++ Phoenix/Phoenix/View/Member/AddMemberInfo.xaml.cs
@@ -15,8 +15,7 @@ namespace Phoenix
 
             mainWindow = mw;
 
-            string url = "https://impro.usercontent.one/appid/oneComWsb/domain/phoenixjudo.dk/media/phoenixjudo.dk/onewebmedia/F%C3%B8nix-logo_collection_Logo%20horisontal%20lille-10.png?etag=%22855d9-670d96f6%22&sourceContentType=image%2Fpng&ignoreAspectRatio&resize=555%2B336";
-            logo.Source = new BitmapImage(new Uri(url, UriKind.Absolute));
+            
 
             IdField.Text = mainWindow.NextMemberID.ToString();
         }
