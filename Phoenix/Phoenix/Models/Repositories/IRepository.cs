1c0ca10fcbc0024378a7c38f3d8781e8878519ff
Oliver Staunstrup <oliver.staunstrup@hotmail.com>
2025-12-03 10:45:57 +0100

    Rettet class locations til; MainMenu & MainWindow ligger i /View samt IRepository ligger i /Models/Repositories

--- Phoenix/Phoenix/Models/Repositories/IRepository.cs
+++ Phoenix/Phoenix/Models/Repositories/IRepository.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Phoenix
+namespace Phoenix.Models.Repositories
 {
     public interface IRepository
     {
--- Phoenix/Phoenix/Models/Repositories/MemberRepository.cs
+++ Phoenix/Phoenix/Models/Repositories/MemberRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Phoenix.Models.Repositories;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
