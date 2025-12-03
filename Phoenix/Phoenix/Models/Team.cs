4f8f104d99cb9ed92b80d4ee7b4c409822e5822c
Darya Skandar <darya.eskandar@gmail.com>
2025-12-03 15:02:32 +0100

    korrekte klasser

--- Phoenix/Phoenix/Models/Team.cs
+++ Phoenix/Phoenix/Models/Team.cs
@@ -1,90 +1,90 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Phoenix
-{
-	public class Team
-	{
+//namespace Phoenix
+//{
+//	public class Team
+//	{
 	
-		public TeamName TeamType { get; set; }
+//		public TeamName TeamType { get; set; }
 		
-        public enum TeamName
-	    { 
-			Motorik,
-			Puslinge,
-			Junior,
-			Senior,
-			BJJ,
+//        public enum TeamName
+//	    { 
+//			Motorik,
+//			Puslinge,
+//			Junior,
+//			Senior,
+//			BJJ,
             
-        }
+//        }
        
 
-        // Backing collections for members and coaches
-        private readonly List<Member> members;
-		private readonly List<Member> coaches;
-		private readonly List<Member> ascoaches;
+//        // Backing collections for members and coaches
+//        private readonly List<Member> members;
+//		private readonly List<Member> coaches;
+//		private readonly List<Member> ascoaches;
 
-		// Price for the team (matches UML)
-		private double price;
+//		// Price for the team (matches UML)
+//		private double price;
         
 
-        // Read-only views for external callers
-        public IReadOnlyList<Member> Members => members.AsReadOnly();
-		public IReadOnlyList<Member> Coaches => coaches.AsReadOnly();
-		public IReadOnlyList<Member> AsCoaches => ascoaches.AsReadOnly();
+//        // Read-only views for external callers
+//        public IReadOnlyList<Member> Members => members.AsReadOnly();
+//		public IReadOnlyList<Member> Coaches => coaches.AsReadOnly();
+//		public IReadOnlyList<Member> AsCoaches => ascoaches.AsReadOnly();
 
-		// Computed properties
-		public int NumMembers => members.Count;
-		public double Price
-		{
-			get => price;
-			set => price = value;
-		}
+//		// Computed properties
+//		public int NumMembers => members.Count;
+//		public double Price
+//		{
+//			get => price;
+//			set => price = value;
+//		}
 
 
-		// Constructor
-		public Team(TeamName teamType)
-		{
+//		// Constructor
+//		public Team(TeamName teamType)
+//		{
 			
-			TeamType = teamType;
-			members = new List<Member>();
-			coaches = new List<Member>();
-			ascoaches = new List<Member>();
-			price = 0.0;
-		}
+//			TeamType = teamType;
+//			members = new List<Member>();
+//			coaches = new List<Member>();
+//			ascoaches = new List<Member>();
+//			price = 0.0;
+//		}
 
 
-        // Methods
-        public int CountMembers()
-		{
-			return members.Count + coaches.Count;
-		}
+//        // Methods
+//        public int CountMembers()
+//		{
+//			return members.Count + coaches.Count;
+//		}
 
-		public void AddMember(Member m)
-		{
-			if (m == null) throw new ArgumentNullException(nameof(m));
-			members.Add(m);
-		}
+//		public void AddMember(Member m)
+//		{
+//			if (m == null) throw new ArgumentNullException(nameof(m));
+//			members.Add(m);
+//		}
 
-		public void AddCoach(Member c)
-		{
-			if (c == null) throw new ArgumentNullException(nameof(c));
-			coaches.Add(c);
-		}
+//		public void AddCoach(Member c)
+//		{
+//			if (c == null) throw new ArgumentNullException(nameof(c));
+//			coaches.Add(c);
+//		}
 
-        public void AddAsCoach(Member s)
-        {
-            if (s == null) throw new ArgumentNullException(nameof(s));
-            ascoaches.Add(s);
-        }
-        public override string ToString()
-        {
-            return TeamType.ToString(); // eller hvad du vil vise
-        }
+//        public void AddAsCoach(Member s)
+//        {
+//            if (s == null) throw new ArgumentNullException(nameof(s));
+//            ascoaches.Add(s);
+//        }
+//        public override string ToString()
+//        {
+//            return TeamType.ToString(); // eller hvad du vil vise
+//        }
 
 
-    }
-}
+//    }
+//}
